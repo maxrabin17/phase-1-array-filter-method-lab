ffb255c1d7c47dc4cd9cb7c3bba8b7dfce916ed1
maxrabin <max.rabin17@gmail.com>
2021-05-03 11:54:36 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution here
+
+        
+/*const driver = drivers.filter(function(val){
+    return drivers(val)
+})*/
+
+
+function findMatching(drivers, string) {
+    return drivers.filter(element => {
+        if (element.toLowerCase() === string.toLowerCase()) {
+            return element
+        }
+    })
+}
+
+function fuzzyMatch(drivers, string) {
+    let x = drivers.filter(element => {
+        return element.startsWith(string)
+    })
+    return x
+}
+
+function matchName(driverObjects, string){
+    let y = driverObjects.filter(element =>{
+        if (element.name === string) {
+            return string
+        }
+    })
+    return y
+}
+
+/* function fuzzyMatch(drivers, string) {
+    let x = drivers.filter(element => {
+         return element.startsWith(string) */
+
+        
